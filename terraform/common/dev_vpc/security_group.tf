7381b676fb6485e8f3d31e709d41aff154d174c9
asbubam <asbubam@smartstudy.co.kr>
2017-09-18 17:27:47 +0900

    add elb-web SG

--- terraform/common/dev_vpc/security_group.tf
+++ terraform/common/dev_vpc/security_group.tf
@@ -43,3 +43,36 @@ resource "aws_security_group" "dev_docker_ephemeral_port_range" {
     Name = "dev-docker-ephemeral-port-range"
   }
 }
+
+resource "aws_security_group" "elb_web" {
+  name        = "elb_web"
+  description = "open http, https port for elb web service"
+  vpc_id      = "${aws_vpc.dev.id}"
+
+  ingress {
+    protocol  = "tcp"
+    self      = true
+    from_port = 80
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol  = "tcp"
+    self      = true
+    from_port = 443
+    to_port   = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "elb-web"
+  }
+}
