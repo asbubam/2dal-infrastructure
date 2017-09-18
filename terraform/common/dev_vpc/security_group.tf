1252b020d513a2f6e7ddb13390829411f028d548
asbubam <asbubam@smartstudy.co.kr>
2017-09-18 15:02:49 +0900

    add docker ephemeral port range

--- terraform/common/dev_vpc/security_group.tf
+++ terraform/common/dev_vpc/security_group.tf
@@ -19,3 +19,27 @@ resource "aws_default_security_group" "dev_default" {
     Name = "dev-default"
   }
 }
+
+resource "aws_security_group" "dev_docker_ephemeral_port_range" {
+  name        = "dev_docker_ephemeral_port_range"
+  description = "open docker ephemeral port range"
+  vpc_id      = "${aws_vpc.dev.id}"
+
+  ingress {
+    protocol  = "tcp"
+    self      = true
+    from_port = 32768
+    to_port   = 61000
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "dev-docker-ephemeral-port-range"
+  }
+}
