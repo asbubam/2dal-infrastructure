eeea9e092d83965ed33fe909daa3fe485c9af497
asbubam <asbubam@smartstudy.co.kr>
2017-10-10 13:41:59 +0900

    Add (default) dhcp_options

---
+++ terraform/common/dev_vpc/dhcp_options.tf
@@ -0,0 +1,10 @@
+resource "aws_default_vpc_dhcp_options" "default" {
+  tags {
+    Name = "default"
+  }
+}
+
+resource "aws_vpc_dhcp_options_association" "dev_default" {
+  vpc_id          = "${aws_vpc.dev.id}"
+  dhcp_options_id = "${aws_default_vpc_dhcp_options.default.id}"
+}
