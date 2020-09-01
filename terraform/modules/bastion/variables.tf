4ba93a40e60f1031ed3c960a8ed0c97aae1fd891
asbubam <asbubam@gmail.com>
2020-09-02 00:42:04 +0900

    Remote double quote for variable types

--- terraform/modules/bastion/variables.tf
+++ terraform/modules/bastion/variables.tf
@@ -1,16 +1,16 @@
 variable "name" {
   description = "모듈에서 정의하는 모든 리소스 이름의 prefix"
-  type        = "string"
+  type        = string
 }
 
 variable "vpc_id" {
   description = "VPC ID"
-  type        = "string"
+  type        = string
 }
 
 variable "ami" {
   description = "bastion 생성에 사용할 AMI"
-  type        = "string"
+  type        = string
 }
 
 variable "instance_type" {
@@ -20,25 +20,25 @@ variable "instance_type" {
 
 variable "availability_zone" {
   description = "bastion EC2 instance availability zone"
-  type        = "string"
+  type        = string
 }
 
 variable "subnet_id" {
   description = "bastion EC2 instance Subnet ID"
-  type        = "string"
+  type        = string
 }
 
 variable "keypair_name" {
   description = "bastion이 사용할 keypair name"
-  type        = "string"
+  type        = string
 }
 
 variable "ingress_cidr_blocks" {
   description = "bastion SSH 접속을 허용할 CIDR block 리스트"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "tags" {
   description = "모든 리소스에 추가되는 tag 맵"
-  type        = "map"
+  type        = map(string)
 }
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,34 +1,34 @@
 variable "name" {
   description = "모듈에서 정의하는 모든 리소스 이름의 prefix"
-  type        = "string"
+  type        = string
 }
 
 variable "cidr" {
   description = "VPC에 할당한 CIDR block"
-  type        = "string"
+  type        = string
 }
 
 variable "public_subnets" {
   description = "Public Subnet IP 리스트"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "private_subnets" {
   description = "Private Subnet IP 리스트"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "database_subnets" {
   description = "Database Subnet IP 리스트"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "azs" {
   description = "사용할 availability zones 리스트"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "tags" {
   description = "모든 리소스에 추가되는 tag 맵"
-  type        = "map"
+  type        = map(string)
 }
